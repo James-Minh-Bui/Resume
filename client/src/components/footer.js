11294583816614bb0d2761f5b4e0c7959fb06b8d
James Bui <james_bui@jamess-air.lan>
2023-09-25 15:44:34 -0700

    Updated footer to work with all pages.

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -1,11 +1,21 @@
-import * as React from 'react';
+import React, { useEffect, useState } from 'react';
 import linkedin from "../assets/LinkedIn.png"
 import git from "../assets/github.png"
+import { useLocation } from 'react-router-dom';
 import "./footer.css"
 
-export default function footer() {
+export default function Footer() {
+  const [ scrollPage, setScrollPage ] = useState();
+  const location = useLocation()
+  useEffect(() => {
+    const currentURL = location.pathname;
+    console.log("this is my url", currentURL)
+    if (currentURL.includes("about") || currentURL.includes("contact")) setScrollPage(true) 
+    else setScrollPage(false)
+  }, [location.pathname]);
+
   return (
-    <div className='footer'>
+    <div className={`footer ${ scrollPage ? 'nonscrollable' : ''}`}>
         <p className='txt'>Copyright James Bui 2023</p>
         <div className='links'>
           <a href='https://github.com/James-Minh-Bui' target='blank'>
